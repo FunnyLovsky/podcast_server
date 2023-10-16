59accbe0f84f04616a856125639ef3d6c4b7f925
Funny_lovsky <crazy.ted.2000@mail.ru>
2023-10-16 20:19:57 +0300

    add static

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const episodes  = require('./data/episode')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000;
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+
 
 app.get('/', (req, res) => {
-    res.status(200).json([{message: 'Working server!'}, {hola: 'a'}])
+    res.status(200).json(episodes)
 })
 
 const start = async () => {
