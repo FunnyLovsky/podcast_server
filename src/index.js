6f40c6c5bd9b0fa7f7ac12de15fc8d9af7f4465b
Funny_lovsky <crazy.ted.2000@mail.ru>
2023-10-18 14:45:10 +0300

    add vercel setting

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
@@ -25,3 +25,5 @@ const start = async () => {
 }
 
 start()
+
+module.exports = app;
